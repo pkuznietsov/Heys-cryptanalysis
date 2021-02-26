2a3514010279a44899e33415c5c065f9a26a8844
Pavel <lwrahgqvjeapokr@gmail.com>
2021-02-26 03:27:19 +0200

    start txt2blocks function

--- include/heys.h
+++ include/heys.h
@@ -9,6 +9,10 @@ private:
 
 public:
 
+    std::vector<block> txt2blocks(std::string path){
+
+    }
+
 
 
     heys(/* args */);
@@ -22,3 +26,39 @@ heys::heys(/* args */)
 heys::~heys()
 {
 }
+
+
+/*
+
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+  char letter ;
+  int i ;
+  string line ;
+
+  ifstream reader( "Shakespeare.txt" ) ;
+
+  if( ! reader ) {
+    cout << "Error opening input file" << endl ;
+    return -1 ;
+  }
+
+  for( i = 0; ! reader.eof() ; i++ ) {
+    reader.get( letter ) ;
+    cout << letter ;
+
+	/*
+    getline( reader , line ) ;
+    cout << line << endl ;
+	*/
+  //}
+
+//  reader.close() ;
+  
+ // return 0 ;
+//}
