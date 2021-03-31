8177ac2d67384c57c22756aa234ca6cd3c1f3853
Pavel <lwrahgqvjeapokr@gmail.com>
2021-03-31 06:20:47 +0300

    dec logic update

--- src/heys.cpp
+++ src/heys.cpp
@@ -53,14 +53,17 @@ int heys::txt_to_data(std::string text_name) {
     }
     reader.close();
 
-    data = chars_to_blocks(chars);
-    std::vector<block> empty_vector(data.size(), 0);
+    input_data = chars_to_blocks(chars);
+
+    std::vector<block> empty_vector(input_data.size(), 0);
     this->encrypted_data = empty_vector;
+    this->decrypted_data = empty_vector;
+    
     return 0;
 }
 
 int heys::data_to_txt(std::string text_name) {
-    data = encrypted_data;
+    std::vector<block> data = encrypted_data;
     std::vector<char> chars = blocks_to_chars(data);
 
     std::string result(chars.begin(), chars.end());
@@ -111,16 +114,13 @@ block heys::enc(block x) {
 } 
 
 int heys::encrypt() {
-    for (int i = 0; i < data.size(); i++) {
-        encrypted_data[i] = enc(data[i]);
+    for (int i = 0; i < input_data.size(); i++) {
+        encrypted_data[i] = enc(input_data[i]);
     }
     return 0;
 }
 
-/***
 block heys::d_round(block temp, int i){
-    temp = temp ^ key[i];
-
     uint16_t mask = 0xF000;
     std::vector<nubbin> nubbins = {};
     for (int i = 3; i >= 0; i--){
@@ -128,33 +128,40 @@ block heys::d_round(block temp, int i){
         mask = mask >> 4;
     }
 
-    for (int i = 0; i < 4; i ++) {
-        nubbins[i] = S_block[nubbins[i]];
-    }
-
     nubbins[0] = (nubbins[0] & 0x8) | ((nubbins[1] & 0x8) >> 1) | ((nubbins[2] & 0x8) >> 2) | ((nubbins[3] & 0x8) >> 3);  
     nubbins[1] = ((nubbins[0] & 0x4) << 1) | (nubbins[1] & 0x4) | ((nubbins[2] & 0x4) >> 1) | ((nubbins[3] & 0x4) >> 2);
     nubbins[2] = ((nubbins[0] & 0x2) << 2) | ((nubbins[1] & 0x2) << 1) | (nubbins[2] & 0x2) | ((nubbins[3] & 0x2) >> 1);
     nubbins[3] = ((nubbins[0] & 0x1) << 3) | ((nubbins[1] & 0x1) << 2) | ((nubbins[2] & 0x1) << 1) | (nubbins[3] & 0x1);
 
-    return ( nubbins[0] << 12 ) | ( nubbins[1] << 8 ) | ( nubbins[2] << 4 ) | ( nubbins[3] );
+    for (int i = 0; i < 4; i ++) {
+        nubbins[i] = S_block_inv[nubbins[i]];
+    }
+
+    temp = ( nubbins[0] << 12 ) | ( nubbins[1] << 8 ) | ( nubbins[2] << 4 ) | ( nubbins[3] );
+    return temp ^ key[i];
 }  
 
 block heys::dec(block x) {
     block result = x;
-    for (int i = 0; i < 7; i++) {
+    for (int i = 6; i >= 0; i--) {
        result = d_round(result,i);
     }
     return result;
-} 
+}
 
 int heys::decrypt() {
-    for (int i = 0; i < data.size(); i++) {
-        encrypted_data[i] = dec(data[i]);
+    for (int i = 0; i < encrypted_data.size(); i++) {
+        decrypted_data[i] = dec(encrypted_data[i]);
     }
     return 0;
 }
-***/
+
+bool heys::verificator() {
+    for (int i = 0; i < input_data.size(); i++) {
+        std::cout << "input_data " << input_data[i] << ":::" << "decrypted_data " << decrypted_data[i] << std::endl;
+    }
+    return input_data == decrypted_data ? true : false;
+}
 
 heys::heys(/* args */)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int main(){
     heys * cipher = new heys();
     auto t2d_error_handler = cipher -> txt_to_data("text.txt");
     auto enc_error_handler = cipher -> encrypt();
+    auto dec_error_handler = cipher -> decrypt();
 
+    auto check_enc_dec = cipher -> verificator();
+    std::cout << check_enc_dec << std::endl;
 
-    auto d2t_error_handler = cipher -> data_to_txt("enc.txt");
+//    auto d2t_error_handler = cipher -> data_to_txt("enc.txt");
+
 
 
     
